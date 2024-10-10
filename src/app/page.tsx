3066dda9a623d3b28f1c72f7c598db2c21b83aed
Demir Dereli <demir.dereli@rapsodo.com>
2024-10-10 14:01:31 +0300

    Added custom fonts and font variables to tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 export default function Home() {
   return (
     <>
-      <div className="bg-primary/20 w-20 h-20">
-        {/* This div has a background of primary color and is 80x80 pixels */}
-      </div>
+      <div className="bg-primary/20 w-20 h-20"></div>
+      <h1 className="text-h1 font-h1">Heading 1</h1>
+      <h2 className="text-h2 font-h2">Heading 2</h2>
+      <h3 className="text-h3 font-h3">Heading 3</h3>
+      <h4 className="text-h4 font-h4">Heading 4</h4>
+      <h5 className="text-h5 font-h5">Heading 5</h5>
+      <h6 className="text-h6 font-h6">Heading 6</h6>
+      <p className="text-p font-p">
+        This is a paragraph with custom font size and font family.
+      </p>
     </>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,6 +16,26 @@ const config: Config = {
         third: "rgb(30, 63, 30, <alpha-value>)", // Third Color: 1E3F1E
         customBlack: "rgb(27, 32, 28, <alpha-value>)", // Custom Black: 1B201C
       },
+      fontFamily: {
+        // Define custom font families
+        p: ["Quicksand", "sans-serif"],
+        h1: ["Oswald", "sans-serif"],
+        h2: ["Oswald", "sans-serif"],
+        h3: ["Oswald", "sans-serif"],
+        h4: ["Oswald", "sans-serif"],
+        h5: ["Oswald", "sans-serif"],
+        h6: ["Oswald", "sans-serif"],
+      },
+      fontSize: {
+        // Define custom font sizes for headers and paragraph
+        p: ["26px", { lineHeight: "1.75rem", fontWeight: "400" }],
+        h1: ["112.3px", { lineHeight: "1.1", fontWeight: "400" }],
+        h2: ["84.2px", { lineHeight: "1.1", fontWeight: "400" }],
+        h3: ["63.2px", { lineHeight: "1.1", fontWeight: "400" }],
+        h4: ["47.4px", { lineHeight: "1.1", fontWeight: "400" }],
+        h5: ["35.5px", { lineHeight: "1.1", fontWeight: "400" }],
+        h6: ["26.7px", { lineHeight: "1.1", fontWeight: "400" }],
+      },
     },
   },
   plugins: [tailwindcssAnimate], // Use the imported plugin
